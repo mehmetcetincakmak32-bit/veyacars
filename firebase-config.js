93e7568a68eeb52a1550a1cd126beaaa5260fbf1
mehmetcetincakmak32-bit <mehmetcetincakmak32@gmail.com>
2026-05-07 03:09:45 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Ayarları
+const firebaseConfig = {
+  apiKey: "BURAYA_API_KEY_GELECEK",
+  authDomain: "veyacars-xxxx.firebaseapp.com",
+  projectId: "veyacars-xxxx",
+  storageBucket: "veyacars-xxxx.appspot.com",
+  messagingSenderId: "xxxxxx",
+  appId: "1:xxxxxx"
+};
+
+// Firebase başlatma ve auth işlemleri bir sonraki adımda eklenecek.
